37461d9627e3a2da2cc0e3203ebd4d9c3ee05da1
S MCty <smcty@github.com>
2023-07-13 19:57:53 -0700

    Rename tomb->bobbin. Delete old supporting code for context.

--- bobbin.go
+++ bobbin.go
@@ -26,33 +26,33 @@
 // NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-// The tomb package handles clean goroutine tracking and termination.
+// The bobbin package handles clean goroutine tracking and termination.
 //
-// The zero value of a Tomb is ready to handle the creation of a tracked
+// The zero value of a Bobbin is ready to handle the creation of a tracked
 // goroutine via its Go method, and then any tracked goroutine may call
 // the Go method again to create additional tracked goroutines at
 // any point.
 //
 // If any of the tracked goroutines returns a non-nil error, or the
 // Kill or Killf method is called by any goroutine in the system (tracked
-// or not), the tomb Err is set, Alive is set to false, and the Dying
+// or not), the bobbin Err is set, Alive is set to false, and the Dying
 // channel is closed to flag that all tracked goroutines are supposed
 // to willingly terminate as soon as possible.
 //
 // Once all tracked goroutines terminate, the Dead channel is closed,
 // and Wait unblocks and returns the first non-nil error presented
-// to the tomb via a result or an explicit Kill or Killf method call,
+// to the bobbin via a result or an explicit Kill or Killf method call,
 // or nil if there were no errors.
 //
 // It is okay to create further goroutines via the Go method while
-// the tomb is in a dying state. The final dead state is only reached
+// the bobbin is in a dying state. The final dead state is only reached
 // once all tracked goroutines terminate, at which point calling
 // the Go method again will cause a runtime panic.
 //
-// Tracked functions and methods that are still running while the tomb
+// Tracked functions and methods that are still running while the bobbin
 // is in dying state may choose to return ErrDying as their error value.
 // This preserves the well established non-nil error convention, but is
-// understood by the tomb as a clean termination. The Err and Wait
+// understood by the bobbin as a clean termination. The Err and Wait
 // methods will still return nil if all observed errors were either
 // nil or ErrDying.
 //
@@ -60,74 +60,74 @@
 //
 //   http://blog.labix.org/2011/10/09/death-of-goroutines-under-control
 //
-package tomb
+package bobbin
 
 import (
-	"errors"
-	"fmt"
-	"sync"
+  "errors"
+  "fmt"
+  "sync"
 )
 
-// A Tomb tracks the lifecycle of one or more goroutines as alive,
+// A Bobbin tracks the lifecycle of one or more goroutines as alive,
 // dying or dead, and the reason for their death.
 //
 // See the package documentation for details.
-type Tomb struct {
-	m      sync.Mutex
-	alive  int
-	dying  chan struct{}
-	dead   chan struct{}
-	reason error
+type Bobbin struct {
+  m      sync.Mutex
+  alive  int
+  dying  chan struct{}
+  dead   chan struct{}
+  reason error
 
-	// context.Context is available in Go 1.7+.
-	parent interface{}
-	child  map[interface{}]childContext
+  // context.Context is available in Go 1.7+.
+  parent interface{}
+  child  map[interface{}]childContext
 }
 
 type childContext struct {
-	context interface{}
-	cancel  func()
-	done    <-chan struct{}
+  context interface{}
+  cancel  func()
+  done    <-chan struct{}
 }
 
 var (
-	ErrStillAlive = errors.New("tomb: still alive")
-	ErrDying      = errors.New("tomb: dying")
+  ErrStillAlive = errors.New("bobbin: still alive")
+  ErrDying      = errors.New("bobbin: dying")
 )
 
-func (t *Tomb) init() {
-	t.m.Lock()
-	if t.dead == nil {
-		t.dead = make(chan struct{})
-		t.dying = make(chan struct{})
-		t.reason = ErrStillAlive
-	}
-	t.m.Unlock()
+func (t *Bobbin) init() {
+  t.m.Lock()
+  if t.dead == nil {
+    t.dead = make(chan struct{})
+    t.dying = make(chan struct{})
+    t.reason = ErrStillAlive
+  }
+  t.m.Unlock()
 }
 
 // Dead returns the channel that can be used to wait until
 // all goroutines have finished running.
-func (t *Tomb) Dead() <-chan struct{} {
-	t.init()
-	return t.dead
+func (t *Bobbin) Dead() <-chan struct{} {
+  t.init()
+  return t.dead
 }
 
 // Dying returns the channel that can be used to wait until
 // t.Kill is called.
-func (t *Tomb) Dying() <-chan struct{} {
-	t.init()
-	return t.dying
+func (t *Bobbin) Dying() <-chan struct{} {
+  t.init()
+  return t.dying
 }
 
 // Wait blocks until all goroutines have finished running, and
 // then returns the reason for their death.
-func (t *Tomb) Wait() error {
-	t.init()
-	<-t.dead
-	t.m.Lock()
-	reason := t.reason
-	t.m.Unlock()
-	return reason
+func (t *Bobbin) Wait() error {
+  t.init()
+  <-t.dead
+  t.m.Lock()
+  reason := t.reason
+  t.m.Unlock()
+  return reason
 }
 
 // Go runs f in a new goroutine and tracks its termination.
@@ -135,7 +135,7 @@ func (t *Tomb) Wait() error {
 // If f returns a non-nil error, t.Kill is called with that
 // error as the death reason parameter.
 //
-// It is f's responsibility to monitor the tomb and return
+// It is f's responsibility to monitor the bobbin and return
 // appropriately once it is in a dying state.
 //
 // It is safe for the f function to call the Go method again
@@ -146,33 +146,33 @@ func (t *Tomb) Wait() error {
 // Calling the Go method after all tracked goroutines return
 // causes a runtime panic. For that reason, calling the Go
 // method a second time out of a tracked goroutine is unsafe.
-func (t *Tomb) Go(f func() error) {
-	t.init()
-	t.m.Lock()
-	defer t.m.Unlock()
-	select {
-	case <-t.dead:
-		panic("tomb.Go called after all goroutines terminated")
-	default:
-	}
-	t.alive++
-	go t.run(f)
+func (t *Bobbin) Go(f func() error) {
+  t.init()
+  t.m.Lock()
+  defer t.m.Unlock()
+  select {
+  case <-t.dead:
+    panic("bobbin.Go called after all goroutines terminated")
+  default:
+  }
+  t.alive++
+  go t.run(f)
 }
 
-func (t *Tomb) run(f func() error) {
-	err := f()
-	t.m.Lock()
-	defer t.m.Unlock()
-	t.alive--
-	if t.alive == 0 || err != nil {
-		t.kill(err)
-		if t.alive == 0 {
-			close(t.dead)
-		}
-	}
+func (t *Bobbin) run(f func() error) {
+  err := f()
+  t.m.Lock()
+  defer t.m.Unlock()
+  t.alive--
+  if t.alive == 0 || err != nil {
+    t.kill(err)
+    if t.alive == 0 {
+      close(t.dead)
+    }
+  }
 }
 
-// Kill puts the tomb in a dying state for the given reason,
+// Kill puts the bobbin in a dying state for the given reason,
 // closes the Dying channel, and sets Alive to false.
 //
 // Althoguh Kill may be called multiple times, only the first
@@ -181,57 +181,57 @@ func (t *Tomb) run(f func() error) {
 // If reason is ErrDying, the previous reason isn't replaced
 // even if nil. It's a runtime error to call Kill with ErrDying
 // if t is not in a dying state.
-func (t *Tomb) Kill(reason error) {
-	t.init()
-	t.m.Lock()
-	defer t.m.Unlock()
-	t.kill(reason)
+func (t *Bobbin) Kill(reason error) {
+  t.init()
+  t.m.Lock()
+  defer t.m.Unlock()
+  t.kill(reason)
 }
 
-func (t *Tomb) kill(reason error) {
-	if reason == ErrStillAlive {
-		panic("tomb: Kill with ErrStillAlive")
-	}
-	if reason == ErrDying {
-		if t.reason == ErrStillAlive {
-			panic("tomb: Kill with ErrDying while still alive")
-		}
-		return
-	}
-	if t.reason == ErrStillAlive {
-		t.reason = reason
-		close(t.dying)
-		for _, child := range t.child {
-			child.cancel()
-		}
-		t.child = nil
-		return
-	}
-	if t.reason == nil {
-		t.reason = reason
-		return
-	}
+func (t *Bobbin) kill(reason error) {
+  if reason == ErrStillAlive {
+    panic("bobbin: Kill with ErrStillAlive")
+  }
+  if reason == ErrDying {
+    if t.reason == ErrStillAlive {
+      panic("bobbin: Kill with ErrDying while still alive")
+    }
+    return
+  }
+  if t.reason == ErrStillAlive {
+    t.reason = reason
+    close(t.dying)
+    for _, child := range t.child {
+      child.cancel()
+    }
+    t.child = nil
+    return
+  }
+  if t.reason == nil {
+    t.reason = reason
+    return
+  }
 }
 
 // Killf calls the Kill method with an error built providing the received
 // parameters to fmt.Errorf. The generated error is also returned.
-func (t *Tomb) Killf(f string, a ...interface{}) error {
-	err := fmt.Errorf(f, a...)
-	t.Kill(err)
-	return err
+func (t *Bobbin) Killf(f string, a ...interface{}) error {
+  err := fmt.Errorf(f, a...)
+  t.Kill(err)
+  return err
 }
 
-// Err returns the death reason, or ErrStillAlive if the tomb
+// Err returns the death reason, or ErrStillAlive if the bobbin
 // is not in a dying or dead state.
-func (t *Tomb) Err() (reason error) {
-	t.init()
-	t.m.Lock()
-	reason = t.reason
-	t.m.Unlock()
-	return
+func (t *Bobbin) Err() (reason error) {
+  t.init()
+  t.m.Lock()
+  reason = t.reason
+  t.m.Unlock()
+  return
 }
 
-// Alive returns true if the tomb is not in a dying or dead state.
-func (t *Tomb) Alive() bool {
-	return t.Err() == ErrStillAlive
+// Alive returns true if the bobbin is not in a dying or dead state.
+func (t *Bobbin) Alive() bool {
+  return t.Err() == ErrStillAlive
 }
---
+++ bobbin_test.go
@@ -0,0 +1,183 @@
+package bobbin_test
+
+import (
+  "errors"
+  bobbin "github.com/smcty/bobbin"
+  "reflect"
+  "testing"
+)
+
+func nothing() error { return nil }
+
+func TestNewTomb(t *testing.T) {
+  tb := &bobbin.Bobbin{}
+  checkState(t, tb, false, false, bobbin.ErrStillAlive)
+}
+
+func TestGo(t *testing.T) {
+  tb := &bobbin.Bobbin{}
+  alive := make(chan bool)
+  tb.Go(func() error {
+    alive <- true
+    tb.Go(func() error {
+      alive <- true
+      <-tb.Dying()
+      return nil
+    })
+    <-tb.Dying()
+    return nil
+  })
+  <-alive
+  <-alive
+  checkState(t, tb, false, false, bobbin.ErrStillAlive)
+  tb.Kill(nil)
+  tb.Wait()
+  checkState(t, tb, true, true, nil)
+}
+
+func TestGoErr(t *testing.T) {
+  first := errors.New("first error")
+  second := errors.New("first error")
+  tb := &bobbin.Bobbin{}
+  alive := make(chan bool)
+  tb.Go(func() error {
+    alive <- true
+    tb.Go(func() error {
+      alive <- true
+      return first
+    })
+    <-tb.Dying()
+    return second
+  })
+  <-alive
+  <-alive
+  tb.Wait()
+  checkState(t, tb, true, true, first)
+}
+
+func TestGoPanic(t *testing.T) {
+  // ErrDying being used properly, after a clean death.
+  tb := &bobbin.Bobbin{}
+  tb.Go(nothing)
+  tb.Wait()
+  defer func() {
+    err := recover()
+    if err != "bobbin.Go called after all goroutines terminated" {
+      t.Fatalf("Wrong panic on post-death bobbin.Go call: %v", err)
+    }
+    checkState(t, tb, true, true, nil)
+  }()
+  tb.Go(nothing)
+}
+
+func TestKill(t *testing.T) {
+  // a nil reason flags the goroutine as dying
+  tb := &bobbin.Bobbin{}
+  tb.Kill(nil)
+  checkState(t, tb, true, false, nil)
+
+  // a non-nil reason now will override Kill
+  err := errors.New("some error")
+  tb.Kill(err)
+  checkState(t, tb, true, false, err)
+
+  // another non-nil reason won't replace the first one
+  tb.Kill(errors.New("ignore me"))
+  checkState(t, tb, true, false, err)
+
+  tb.Go(nothing)
+  tb.Wait()
+  checkState(t, tb, true, true, err)
+}
+
+func TestKillf(t *testing.T) {
+  tb := &bobbin.Bobbin{}
+
+  err := tb.Killf("BO%s", "OM")
+  if s := err.Error(); s != "BOOM" {
+    t.Fatalf(`Killf("BO", "OM"): want "BOOM", got %q`, s)
+  }
+  checkState(t, tb, true, false, err)
+
+  // another non-nil reason won't replace the first one
+  tb.Killf("ignore me")
+  checkState(t, tb, true, false, err)
+
+  tb.Go(nothing)
+  tb.Wait()
+  checkState(t, tb, true, true, err)
+}
+
+func TestErrDying(t *testing.T) {
+  // ErrDying being used properly, after a clean death.
+  tb := &bobbin.Bobbin{}
+  tb.Kill(nil)
+  tb.Kill(bobbin.ErrDying)
+  checkState(t, tb, true, false, nil)
+
+  // ErrDying being used properly, after an errorful death.
+  err := errors.New("some error")
+  tb.Kill(err)
+  tb.Kill(bobbin.ErrDying)
+  checkState(t, tb, true, false, err)
+
+  // ErrDying being used badly, with an alive bobbin.
+  tb = &bobbin.Bobbin{}
+  defer func() {
+    err := recover()
+    if err != "bobbin: Kill with ErrDying while still alive" {
+      t.Fatalf("Wrong panic on Kill(ErrDying): %v", err)
+    }
+    checkState(t, tb, false, false, bobbin.ErrStillAlive)
+  }()
+  tb.Kill(bobbin.ErrDying)
+}
+
+func TestKillErrStillAlivePanic(t *testing.T) {
+  tb := &bobbin.Bobbin{}
+  defer func() {
+    err := recover()
+    if err != "bobbin: Kill with ErrStillAlive" {
+      t.Fatalf("Wrong panic on Kill(ErrStillAlive): %v", err)
+    }
+    checkState(t, tb, false, false, bobbin.ErrStillAlive)
+  }()
+  tb.Kill(bobbin.ErrStillAlive)
+}
+
+func checkState(t *testing.T, tb *bobbin.Bobbin, wantDying, wantDead bool, wantErr error) {
+  select {
+  case <-tb.Dying():
+    if !wantDying {
+      t.Error("<-Dying: should block")
+    }
+  default:
+    if wantDying {
+      t.Error("<-Dying: should not block")
+    }
+  }
+  seemsDead := false
+  select {
+  case <-tb.Dead():
+    if !wantDead {
+      t.Error("<-Dead: should block")
+    }
+    seemsDead = true
+  default:
+    if wantDead {
+      t.Error("<-Dead: should not block")
+    }
+  }
+  if err := tb.Err(); err != wantErr {
+    t.Errorf("Err: want %#v, got %#v", wantErr, err)
+  }
+  if wantDead && seemsDead {
+    waitErr := tb.Wait()
+    switch {
+    case waitErr == bobbin.ErrStillAlive:
+      t.Errorf("Wait should not return ErrStillAlive")
+    case !reflect.DeepEqual(waitErr, wantErr):
+      t.Errorf("Wait: want %#v, got %#v", wantErr, waitErr)
+    }
+  }
+}
